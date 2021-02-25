81d3dfeb9a8fc322997c37b5cb854df4e08d1963
João <jovic182015@gmail.com>
2021-02-25 11:03:15 -0300

    fim do segundo dia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import Head from 'next/head'
 import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Coutdown } from  '../components/Countdown'
+
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container"> 
-        <head>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
-        </head>
+    <div className={styles.container}> 
+      <Head>
+        <title>inicio: moveit</title>
+      </Head>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges/>
+          <Coutdown/>
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>  );  
 }
